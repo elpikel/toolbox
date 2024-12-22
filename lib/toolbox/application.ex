d1e56f44a3fc17122d1c707eaa2c627434350fac
Arkadiusz Plichta <el.pikel@gmail.com>
2024-12-22 20:23:26 +0100

    init

---
+++ lib/toolbox/application.ex
@@ -0,0 +1,36 @@
+defmodule Toolbox.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      ToolboxWeb.Telemetry,
+      Toolbox.Repo,
+      {DNSCluster, query: Application.get_env(:toolbox, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: Toolbox.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: Toolbox.Finch},
+      # Start a worker by calling: Toolbox.Worker.start_link(arg)
+      # {Toolbox.Worker, arg},
+      # Start to serve requests, typically the last entry
+      ToolboxWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Toolbox.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    ToolboxWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ lib/toolbox/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Toolbox.Mailer do
+  use Swoosh.Mailer, otp_app: :toolbox
+end
